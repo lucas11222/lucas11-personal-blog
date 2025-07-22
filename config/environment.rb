9b878f9c130511920153df8d892f71bbd125562f
BuildTools <altamiranolucas2013@gmail.com>
2025-07-22 16:25:09 +0200

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
-require_relative "application"
-puts "🔍 RAILS_MASTER_KEY=#{ENV['RAILS_MASTER_KEY']&.slice(0, 5)}..."
+##require_relative "application"
 # Initialize the Rails application.
-Rails.application.initialize!
+##Rails.application.initialize!
+# config/application.rb
+require_relative "boot"
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module Lucas11PersonalBlog
+  class Application < Rails::Application
+    config.load_defaults 7.0
+  end
+end
